f564c809caac873f64c6296652d1cbd1358aafb9
Martin Jaskulla <martinjaskulla@icloud.com>
2018-12-18 10:51:58 +0100

    SelectWorkoutPage responsive done

--- src/components/pages/SelectWorkoutPage.js
+++ src/components/pages/SelectWorkoutPage.js
@@ -4,18 +4,20 @@ import styled from "styled-components"
 const SelectWorkoutPage = props => {
     return (
         <Layout>
-            <H1>Choose your Workout!</H1>
-            <Div>
-                <ButtonBox>
-                    <Button onClick={() => props.history.push("/workouts/entry")}><span role="img" aria-label="Hatching chick">🐣</span></Button>
-                </ButtonBox>
-                <ButtonBox>
-                    <Button onClick={() => props.history.push("/workouts/intermediate")}><span role="img" aria-label="Monkey">🐒</span></Button>
-                </ButtonBox>
-                <ButtonBox>
-                    <Button onClick={() => props.history.push("/workouts/advanced")}><span role="img" aria-label="Gorilla">🦍</span></Button>
-                </ButtonBox>
-            </Div>
+            <CenterBox>
+                <H1>Choose your Workout!</H1>
+                <div>
+                    <ButtonBox>
+                        <Button onClick={() => props.history.push("/workouts/entry")}><span role="img" aria-label="Hatching chick">🐣</span></Button>
+                    </ButtonBox>
+                    <ButtonBox>
+                        <Button onClick={() => props.history.push("/workouts/intermediate")}><span role="img" aria-label="Monkey">🐒</span></Button>
+                    </ButtonBox>
+                    <ButtonBox>
+                        <Button onClick={() => props.history.push("/workouts/advanced")}><span role="img" aria-label="Gorilla">🦍</span></Button>
+                    </ButtonBox>
+                </div>
+            </CenterBox>
         </Layout>
     )
 }
@@ -26,44 +28,34 @@ padding: 5vh;
 box-sizing: border-box;
 background: #1fb990;
 position: relative;
+text-align: center
 `
+
+const CenterBox = styled.div`
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+width: 80vw;
+`
+
 const H1 = styled.h1`
-height: 45vh;
-line-height: 50vh;
-text-align: center;
 font-family: Bangers, Arial;
 font-size: 8vw;
 font-weight: 100;
 color: #fff;
 margin: 0rem;
-`
-
-const Div = styled.div`
-height: 45vh;
-width: 65vw;
-position: absolute;
-left: 50%;
-transform: translateX(-50%);
+margin-bottom: 10vh;
 `
 
 const ButtonBox = styled.div`
 display: inline-block;
-position: absolute;
-top: 50%;
-transform: translateY(-50%);
-:nth-child(1) {
-    
-}
-:nth-child(2) {
-    left: 50%;
-    transform: translate(-50%, -50%);
-}
-:nth-child(3) {
-    right: 0;
-    transform: translate(0, -50%);
+outline: none;
+
+:not(:last-child) {
+    margin-right: 14vw;
 }
 
-outline: none;
 &:hover > * {
     transform: translateY(-1rem) scale(1.02 )
 } 
