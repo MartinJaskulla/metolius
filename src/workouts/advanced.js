b5572fb6e4bb48357b4eca07489c02a4b4d36d6f
Martin Jaskulla <martinjaskulla@icloud.com>
2018-12-18 10:58:37 +0100

    added workouts

---
+++ src/workouts/advanced.js
@@ -0,0 +1,163 @@
+export default [
+    // 1st min
+    {
+        description: "Dead Hang",
+        hold: "Six",
+        seconds: 25,
+    },
+    {
+        description: "5 Pull-ups",
+        hold: "Nine",
+        seconds: 35,
+    },
+    // 2nd min
+    {
+        description: "5 Pull-ups",
+        hold: "Fifteen TwelveLeft",
+        seconds: 30,
+    },
+    {
+        description: "5 Pull-ups",
+        hold: "Fifteen TwelveRight",
+        seconds: 30,
+    },
+
+    // 3rd min
+    {
+        description: "Dead Hang",
+        hold: "Eleven",
+        seconds: 45,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 15,
+    },
+    // 4th min
+    {
+        description: "5 Pull-ups",
+        hold: "ThreeLeft FourRight",
+        seconds: 30,
+    },
+    {
+        description: "5 Pull-ups",
+        hold: "ThreeRight FourLeft",
+        seconds: 30,
+    },
+    // 5th min
+    {
+        description: "Dead Hang",
+        hold: "Eleven",
+        seconds: 10,
+    },
+    {
+        description: "Campus",
+        hold: "Nine",
+        seconds: 5,
+    },
+    {
+        description: "Campus",
+        hold: "Six",
+        seconds: 5,
+    },
+    {
+        description: "Campus",
+        hold: "Two",
+        seconds: 5,
+    },
+    {
+        description: "Dead Hang",
+        hold: "Two",
+        seconds: 15,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 20,
+    },
+    // 6th min
+    {
+        description: "One Arm Hang",
+        hold: "ThreeLeft",
+        seconds: 15,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 10,
+    },
+    {
+        description: "One Arm Hang",
+        hold: "ThreeRight",
+        seconds: 15,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 20,
+    },
+    // 7th min
+    {
+        description: "5 L-sit Pull-ups",
+        hold: "One",
+        seconds: 20,
+    },
+    {
+        description: "Bent Arm Hang",
+        hold: "Twelve",
+        seconds: 20,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 20,
+    },
+    // 8th min
+    {
+        description: "Bent Arm Hang",
+        hold: "Eight",
+        seconds: 20,
+    },
+    {
+        description: "Campus",
+        hold: "9", //x-deep three finger pockets?
+        seconds: 5,
+    },
+    {
+        description: "Dead Hang",
+        hold: "9", //x-deep three finger pockets?
+        seconds: 25,
+    },
+    // 9th min
+    {
+        description: "Dead Hang",
+        hold: "Eighteen Seventeen",
+        seconds: 10,
+    },
+    {
+        description: "Reverse Holds & Dead Hang",
+        hold: "Eighteen Seventeen",
+        seconds: 10,
+    },
+    {
+        description: "3 Pull-ups",
+        hold: "Eighteen Seventeen",
+        seconds: 20,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 20,
+    },
+    // 10th min
+    {
+        description: "8 Pull-ups",
+        hold: "One",
+        seconds: 25,
+    },
+    {
+        description: "Dead Hang",
+        hold: "Three",
+        seconds: 35,
+    },
+]
---
+++ src/workouts/intermediate.js
@@ -0,0 +1,133 @@
+export default [
+    // 1st min
+    {
+        description: "Dead Hang",
+        hold: "Five",
+        seconds: 25,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 35,
+    },
+    // 2nd min
+    {
+        description: "Dead Hang",
+        hold: "Two",
+        seconds: 20,
+    },
+    {
+        description: "3 Pull-ups",
+        hold: "Two",
+        seconds: 40,
+    },
+
+    // 3rd min
+    {
+        description: "Bent Arm Hang",
+        hold: "Six",
+        seconds: 15,
+    },
+    {
+        description: "10 Knee Raises",
+        hold: "One",
+        seconds: 45,
+    },
+    // 4th min
+    {
+        description: "Dead Hang",
+        hold: "Two",
+        seconds: 15,
+    },
+    {
+        description: "Dead Hang",
+        hold: "Three",
+        seconds: 15,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 30,
+    },
+    // 5th min
+    {
+        description: "Dead Hang",
+        hold: "One SeventeenLeft",
+        seconds: 20,
+    },
+    {
+        description: "Dead Hang",
+        hold: "One SeventeenRight",
+        seconds: 20,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 20,
+    },
+    // 6th min
+    {
+        description: "Dead Hang",
+        hold: "FourLeft NineRight",
+        seconds: 15,
+    },
+    {
+        description: "Dead Hang",
+        hold: "FourRight NineLeft",
+        seconds: 15,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 30,
+    },
+    // 7th min
+    {
+        description: "4 Pull-ups",
+        hold: "Five",
+        seconds: 25,
+    },
+    {
+        description: "10 Knee Raises",
+        hold: "Any",
+        seconds: 35,
+    },
+    // 8th min
+    {
+        description: "Dead Hang",
+        hold: "Seven",
+        seconds: 30,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 30,
+    },
+    // 9th min
+    {
+        description: "One Arm Hang",
+        hold: "OneLeft",
+        seconds: 10,
+    },
+    {
+        description: "One Arm Hang",
+        hold: "OneRight",
+        seconds: 10,
+    },
+    {
+        description: "Rest",
+        hold: null,
+        seconds: 40,
+    },
+    // 10th min
+    {
+        description: "5 Pull-ups",
+        hold: "Seven",
+        seconds: 25,
+    },
+    {
+        description: "Dead Hang",
+        hold: "Three",
+        seconds: 35,
+    },
+]
